1afbb709bf6346017c52e1ac8dabad911d972c11
yudaprima <myroofel@gmail.com>
2021-07-26 14:04:07 +0700

    update home

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-white">
     <Hero />
-    <ProductServices />
     <TwoCard />
+    <ProductServices />
     <!-- <TwoCardRight /> -->
     <Banner />
     <Testimonial />
