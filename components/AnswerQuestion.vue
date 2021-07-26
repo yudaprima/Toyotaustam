5c48070dd7b7420e9ec46d746bbb818da75edf7a
yudaprima <myroofel@gmail.com>
2021-07-26 16:27:16 +0700

    update IG

--- components/AnswerQuestion.vue
+++ components/AnswerQuestion.vue
@@ -71,7 +71,7 @@
               Please don't hesitate to contact us for another task
             </p>
             <p class="text-sm pt-2 text-lg">
-              Phone 628122687656 <br />Email | hendiantony0309@gmail.com
+              Phone 628122687656 <br />Instagram | oestamdwi_salestoyotajogja
             </p>
             <a
               href="https://api.whatsapp.com/send?phone=+*628122687656*&text=%20*Saya ingin menanyakan tentang mobil..."
