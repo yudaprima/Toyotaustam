c969a39d0e220e87114cf29cec435be15e3565ee
yudaprima <myroofel@gmail.com>
2021-08-31 07:31:50 +0700

    change map

--- components/AnswerQuestion.vue
+++ components/AnswerQuestion.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="container mx-auto ">
+  <div class="container mx-auto">
     <div
-      class="flex flex-wrap px-3 lg:px-16 pt-32 grid lg:grid-cols-2 lg:gap-16 lg:pb-16 justify-between"
+      class="
+        flex flex-wrap
+        px-3
+        lg:px-16
+        pt-32
+        grid
+        lg:grid-cols-2 lg:gap-16 lg:pb-16
+        justify-between
+      "
     >
       <!-- Component Start -->
       <div class="w-full max-w-screen-sm">
         <button
-          class="w-full border-b-2 border-gray-400 pb-6 text-left group mt-6 focus:outline-none"
+          class="
+            w-full
+            border-b-2 border-gray-400
+            pb-6
+            text-left
+            group
+            mt-6
+            focus:outline-none
+          "
         >
           <div class="text-lg font-semibold">
             Q: Apakah beli mobil harus ke dealer ?
@@ -19,11 +35,17 @@
           </div>
         </button>
         <button
-          class="w-full border-b-2 border-gray-400 pb-6 text-left group mt-6 focus:outline-none"
+          class="
+            w-full
+            border-b-2 border-gray-400
+            pb-6
+            text-left
+            group
+            mt-6
+            focus:outline-none
+          "
         >
-          <div class="text-lg font-semibold">
-            Q: Apakah bisa tukar tambah ?
-          </div>
+          <div class="text-lg font-semibold">Q: Apakah bisa tukar tambah ?</div>
           <div class="mt-3 hidden group-focus:flex">
             <p>
               Bisa sekali, jika anda sudah memiliki mobil toyota lama dan ingin
@@ -33,7 +55,15 @@
           </div>
         </button>
         <button
-          class="w-full border-b-2 border-gray-400 pb-6 text-left group mt-6 focus:outline-none"
+          class="
+            w-full
+            border-b-2 border-gray-400
+            pb-6
+            text-left
+            group
+            mt-6
+            focus:outline-none
+          "
         >
           <div class="text-lg font-semibold">
             Q: Apakah ada promo untuk pembelian toyota ?
@@ -47,7 +77,15 @@
           </div>
         </button>
         <button
-          class="w-full border-b-2 border-gray-400 pb-6 text-left group mt-6 focus:outline-none"
+          class="
+            w-full
+            border-b-2 border-gray-400
+            pb-6
+            text-left
+            group
+            mt-6
+            focus:outline-none
+          "
         >
           <div class="text-lg font-semibold">
             Q: Untuk pembelian kredit apakah mudah dan bisa dibantu?
@@ -60,8 +98,8 @@
           </div>
         </button>
       </div>
-      <div class=" lg:text-right mt-4 lg:mt-0">
-        <div class="md:py-4 flex-grow flex flex-col space-y-8 ">
+      <div class="lg:text-right mt-4 lg:mt-0">
+        <div class="md:py-4 flex-grow flex flex-col space-y-8">
           <div class="">
             <h1 class="font-bold text-4xl tracking-wide text-red">
               Contact Us
@@ -73,16 +111,22 @@
               Phone 628122687656 <br />Instagram | oestamdwi_salestoyotajogja
               <br />Email | oestamdwikaryadi@gmail.com
             </p> -->
-            <p class="text-sm pt-2 text-lg">
-              Phone 6287739005888
-            </p>
+            <p class="text-sm pt-2 text-lg">Phone 6287739005888</p>
             <a
               href="https://api.whatsapp.com/send?phone=+*6287739005888*&text=%20*Saya ingin menanyakan tentang mobil..."
               target="_blank"
               rel="noopener noreferrer"
             >
               <button
-                class="rounded-lg py-2 px-8 text-xl text-red text-center border-2 border-red hover:bg-red hover:text-white mt-9"
+                class="
+                  rounded-lg
+                  py-2
+                  px-8
+                  text-xl text-red text-center
+                  border-2 border-red
+                  hover:bg-red hover:text-white
+                  mt-9
+                "
               >
                 Contact WhatsApp
               </button>
@@ -90,12 +134,21 @@
           </div>
         </div>
         <a
-          href="https://goo.gl/maps/agdUvVcKF9T52LzT6"
+          href="https://www.google.com/maps/dir/-7.6762044,109.652039/pusat+penjualan+toyota+jogja/@-7.8142056,109.4698272,9z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x2e7a59f72dd446d5:0xe1521901b407c7fc!2m2!1d110.4090148!2d-7.8098156"
           target="_blank"
           rel="noopener noreferrer"
         >
           <button
-            class="rounded-lg py-2 px-6 text-xl text-red text-center border-2 border-red hover:bg-red hover:text-white mt-5 mb-5"
+            class="
+              rounded-lg
+              py-2
+              px-6
+              text-xl text-red text-center
+              border-2 border-red
+              hover:bg-red hover:text-white
+              mt-5
+              mb-5
+            "
           >
             check in Google Map
           </button>
