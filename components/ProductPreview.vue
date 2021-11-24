0e42a3ed3ca2193b9215efae17cb39875a3e0e5b
yudaprima <myroofel@gmail.com>
2021-11-24 23:14:18 +0700

    update price nov

--- components/ProductPreview.vue
+++ components/ProductPreview.vue
@@ -1,36 +1,58 @@
 <template>
   <div class="w-full mx-auto p-3 lg:pt-8 pb-7 mt-9">
     <div class="lg:space-y-6">
-      <div class=" flex flex-wrap mx-auto lg:px-2 ">
+      <div class="flex flex-wrap mx-auto lg:px-2">
         <div class="w-full">
           <div class="flex flex-wrap justify-center grid lg:grid-cols-2 gap-3">
             <div
-              class=" rounded-lg mx-2 my-5 space-y-6 mx-auto flex flex-col md:flex-row"
+              class="
+                rounded-lg
+                mx-2
+                my-5
+                space-y-6
+                mx-auto
+                flex flex-col
+                md:flex-row
+              "
               v-for="product in products"
               :key="product.id"
             >
               <div
                 class="flex flex-col w-full lg:w-3/5 justify-center items-start"
               >
-                <h1 class=" mb-5 text-red font-extrabold ">
+                <h1 class="mb-5 text-red font-extrabold">
                   {{ product.title }}
                 </h1>
                 <nuxt-img
                   v-if="product.image"
-                  class=" object-cover rounded-lg"
+                  class="object-cover rounded-lg"
                   :src="product.image"
                   width="500px"
                   height="290px"
                 />
                 <button
-                  class="w-full border-b-2 border-gray-800 pb-4 text-left group mt-4 focus:outline-none"
+                  class="
+                    w-full
+                    border-b-2 border-gray-800
+                    pb-4
+                    text-left
+                    group
+                    mt-4
+                    focus:outline-none
+                  "
                 >
-                  <div class=" font-semibold text-red  mb-5">
-                    PRICE
-                  </div>
+                  <div class="font-semibold text-red mb-5">PRICE</div>
 
                   <p
-                    class="pt-3 justify-between text-red mb-4 flex flex-wrap hidden group-focus:flex"
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     {{ product.price }}
                     <!-- <span
@@ -42,79 +64,260 @@
                   </p>
 
                   <p
-                    class="pt-3 justify-between text-red mb-4 flex flex-wrap hidden group-focus:flex"
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     <span class="italic font-light">{{ product.price2 }}</span>
                   </p>
                   <p
-                    class="pt-3 justify-between text-red mb-4 flex flex-wrap hidden group-focus:flex"
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     <span class="italic font-light">{{ product.price3 }}</span>
                   </p>
                   <p
-                    class="pt-3 justify-between text-red mb-4 flex flex-wrap hidden group-focus:flex"
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     <span class="italic font-light">{{ product.price4 }}</span>
                   </p>
                   <p
-                    class="pt-3 justify-between text-red mb-4 flex flex-wrap hidden group-focus:flex"
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     <span class="italic font-light">{{ product.price5 }}</span>
                   </p>
                   <p
-                    class="pt-3 justify-between text-red mb-4 flex flex-wrap hidden group-focus:flex"
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     <span class="italic font-light">{{ product.price6 }}</span>
                   </p>
                   <p
-                    class="pt-3 justify-between text-red mb-4 flex flex-wrap hidden group-focus:flex"
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     <span class="italic font-light">{{ product.price7 }}</span>
                   </p>
                   <p
-                    class="pt-3 justify-between text-red mb-4 flex flex-wrap hidden group-focus:flex"
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     <span class="italic font-light">{{ product.price8 }}</span>
                   </p>
                   <p
-                    class="pt-3 justify-between text-red mb-4 flex flex-wrap hidden group-focus:flex"
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     <span class="italic font-light">{{ product.price9 }}</span>
                   </p>
                   <p
-                    class="pt-3 justify-between text-red mb-4 flex flex-wrap hidden group-focus:flex"
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     <span class="italic font-light">{{ product.price10 }}</span>
                   </p>
-                </button>
-              </div>
-              <div class="p-2 bg-gray-900 text-white rounded-lg  lg:w-2/5">
-                <button
-                  class="w-full border-b-2 border-gray-800 pb-4 text-left group mt-4 focus:outline-none"
-                >
-                  <div class=" font-semibold mb-5">
-                    DIMENSI
-                  </div>
 
                   <p
-                    class="  text-sm text-gray-400 mb-4 font-bold hidden group-focus:flex"
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
+                  >
+                    <span class="italic font-light">{{ product.price11 }}</span>
+                  </p>
+                  <p
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
+                  >
+                    <span class="italic font-light">{{ product.price12 }}</span>
+                  </p>
+                  <p
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
+                  >
+                    <span class="italic font-light">{{ product.price13 }}</span>
+                  </p>
+                  <p
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
+                  >
+                    <span class="italic font-light">{{ product.price14 }}</span>
+                  </p>
+                  <p
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
+                  >
+                    <span class="italic font-light">{{ product.price15 }}</span>
+                  </p>
+                  <p
+                    class="
+                      pt-3
+                      justify-between
+                      text-red
+                      mb-4
+                      flex flex-wrap
+                      hidden
+                      group-focus:flex
+                    "
+                  >
+                    <span class="italic font-light">{{ product.price16 }}</span>
+                  </p>
+                </button>
+              </div>
+              <div class="p-2 bg-gray-900 text-white rounded-lg lg:w-2/5">
+                <button
+                  class="
+                    w-full
+                    border-b-2 border-gray-800
+                    pb-4
+                    text-left
+                    group
+                    mt-4
+                    focus:outline-none
+                  "
+                >
+                  <div class="font-semibold mb-5">DIMENSI</div>
+
+                  <p
+                    class="
+                      text-sm text-gray-400
+                      mb-4
+                      font-bold
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     L / W / H / Wheelbase :
                   </p>
                   <p
-                    class="  text-sm text-gray-400 mb-4 font-bold hidden group-focus:flex"
+                    class="
+                      text-sm text-gray-400
+                      mb-4
+                      font-bold
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     <span class="italic font-light">{{ product.Dimensi }}</span>
                   </p>
                 </button>
                 <button
-                  class="w-full border-b-2 border-gray-800 pb-4 text-left group mt-4 focus:outline-none"
+                  class="
+                    w-full
+                    border-b-2 border-gray-800
+                    pb-4
+                    text-left
+                    group
+                    mt-4
+                    focus:outline-none
+                  "
                 >
-                  <div class=" font-semibold mb-5">
-                    SPESIFIKASI
-                  </div>
+                  <div class="font-semibold mb-5">SPESIFIKASI</div>
                   <p
-                    class="  text-sm text-gray-400 mb-4 font-bold hidden group-focus:flex"
+                    class="
+                      text-sm text-gray-400
+                      mb-4
+                      font-bold
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     Transmission :
                     <span class="italic font-light">{{
@@ -122,7 +325,13 @@
                     }}</span>
                   </p>
                   <p
-                    class="  text-sm text-gray-400 mb-4 font-bold hidden group-focus:flex"
+                    class="
+                      text-sm text-gray-400
+                      mb-4
+                      font-bold
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     BrakeSystem :
                     <span class="italic font-light">{{
@@ -130,7 +339,13 @@
                     }}</span>
                   </p>
                   <p
-                    class="  text-sm text-gray-400 mb-4 font-bold hidden group-focus:flex"
+                    class="
+                      text-sm text-gray-400
+                      mb-4
+                      font-bold
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     FrontBrake :
                     <span class="italic font-light">{{
@@ -138,7 +353,13 @@
                     }}</span>
                   </p>
                   <p
-                    class="  text-sm text-gray-400 mb-4 font-bold hidden group-focus:flex"
+                    class="
+                      text-sm text-gray-400
+                      mb-4
+                      font-bold
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     RearBrake :
                     <span class="italic font-light">{{
@@ -146,7 +367,13 @@
                     }}</span>
                   </p>
                   <p
-                    class="  text-sm text-gray-400 mb-4 font-bold hidden group-focus:flex"
+                    class="
+                      text-sm text-gray-400
+                      mb-4
+                      font-bold
+                      hidden
+                      group-focus:flex
+                    "
                   >
                     FrontSuspension :
                     <span class="italic font-light">{{
@@ -155,26 +382,50 @@
                   </p>
                 </button>
                 <button
-                  class="w-full border-b-2 border-gray-800 pb-4 text-left group mt-4 focus:outline-none"
+                  class="
+                    w-full
+                    border-b-2 border-gray-800
+                    pb-4
+                    text-left
+                    group
+                    mt-4
+                    focus:outline-none
+                  "
                 >
-                  <div class=" font-semibold mb-5">
-                    CHASSIS
-                  </div>
+                  <div class="font-semibold mb-5">CHASSIS</div>
                   <div>
                     <p
-                      class="  text-sm text-gray-400 mb-4 font-bold hidden group-focus:flex"
+                      class="
+                        text-sm text-gray-400
+                        mb-4
+                        font-bold
+                        hidden
+                        group-focus:flex
+                      "
                     >
                       Engine Type :
                     </p>
                     <p
-                      class="  text-sm text-gray-400 mb-4 font-bold hidden group-focus:flex"
+                      class="
+                        text-sm text-gray-400
+                        mb-4
+                        font-bold
+                        hidden
+                        group-focus:flex
+                      "
                     >
                       <span class="italic font-light">{{
                         product.EngineType
                       }}</span>
                     </p>
                     <p
-                      class="  text-sm text-gray-400 mb-4 font-bold hidden group-focus:flex"
+                      class="
+                        text-sm text-gray-400
+                        mb-4
+                        font-bold
+                        hidden
+                        group-focus:flex
+                      "
                     >
                       Bore x Stroke :
                       <span class="italic font-light">{{
@@ -182,7 +433,13 @@
                       }}</span>
                     </p>
                     <p
-                      class="  text-sm text-gray-400 mb-4 font-bold hidden group-focus:flex"
+                      class="
+                        text-sm text-gray-400
+                        mb-4
+                        font-bold
+                        hidden
+                        group-focus:flex
+                      "
                     >
                       Displacement :
                       <span class="italic font-light">{{
@@ -190,7 +447,13 @@
                       }}</span>
                     </p>
                     <p
-                      class="  text-sm text-gray-400 mb-4 font-bold hidden group-focus:flex"
+                      class="
+                        text-sm text-gray-400
+                        mb-4
+                        font-bold
+                        hidden
+                        group-focus:flex
+                      "
                     >
                       Max Power :
                       <span class="italic font-light">{{
@@ -198,7 +461,13 @@
                       }}</span>
                     </p>
                     <p
-                      class="  text-sm text-gray-400 mb-4 font-bold hidden group-focus:flex"
+                      class="
+                        text-sm text-gray-400
+                        mb-4
+                        font-bold
+                        hidden
+                        group-focus:flex
+                      "
                     >
                       Max Torque :
                       <span class="italic font-light">{{
@@ -221,7 +490,7 @@
 export default {
   name: "products",
   components: {},
-  props: ["products"]
+  props: ["products"],
 };
 </script>
 <style>
